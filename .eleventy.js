80c6a7ea6f4c87d15849a20a3f9d026449e49497
Ahmad Abdullah <ahmadnofly@gmail.com>
2022-06-09 23:47:50 +0300

    :rocket: first deploy
    Its pushing to netlify now

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.ignores.add("./src/_sass");
+    eleventyConfig.ignores.add("./src/_css/main.css.map");
+  
+    eleventyConfig.addWatchTarget("./src/css");
+    eleventyConfig.addPassthroughCopy({ "./src/_css": "/css" });
+    eleventyConfig.addPassthroughCopy({ "./src/_fonts": "/fonts" });
+    eleventyConfig.addPassthroughCopy({ "./src/_public": "/" });
+  
+    eleventyConfig.addPassthroughCopy({ "./src/_data/images": "/images" });
+
+    return {
+      dir: {
+        data: "_data",
+        input: "src",
+        includes: "_includes",
+        layouts: "_layouts",
+      },
+    };
+  };
---
+++ src/index.md
@@ -0,0 +1,9 @@
+---
+layout: base.html
+title: Classic Media Production
+metaDescription:
+  Classic Media Production provides exceptional printing services by pursuing business through innovation and creativity that exceed the expectation of our esteemed customers.
+templateEngineOverride: njk
+---
+
+yolo
